84ea3d7c518511c6717a2139ddcce416f398fb64
nguyendinhhieu12345 <dinhhieunguyen07@gmail.com>
2023-06-08 21:39:35 +0700

    update CRUD

--- src/main/java/com/example/demo/controller/productController.java
+++ src/main/java/com/example/demo/controller/productController.java
@@ -1,21 +1,89 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Product;
+import com.example.demo.model.ResponseObject;
 import com.example.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/v1/Products")
 public class productController {
     @Autowired
     private ProductRepository repository;
+
     @GetMapping("")
     List<Product> getAllProducts() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<ResponseObject> findById(@PathVariable Long id) {
+        Optional<Product> foundProduct = repository.findById(id);
+        if (foundProduct.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("OK", "OK", foundProduct)
+            );
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ResponseObject("fail", "Cannot find id", "")
+            );
+        }
+    }
+
+    @PostMapping("/insert")
+    ResponseEntity<ResponseObject> insertProduct(@RequestBody Product newProduct) {
+        List<Product> findProductName = repository.findByProductName(newProduct.getProductName());
+        if (findProductName.size() > 0) {
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                    new ResponseObject("fail", "Trung ten", "")
+            );
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("OK", "OK", repository.save(newProduct))
+            );
+        }
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<ResponseObject> updateProduct(@RequestBody Product newProduct, @PathVariable Long id) {
+        Product updateProduct = repository.findById(id).map(
+                product -> {
+                    product.setProductName(newProduct.getProductName());
+                    product.setProductYear(newProduct.getProductYear());
+                    product.setPrice(newProduct.getPrice());
+                    product.setUrl(newProduct.getUrl());
+                    return repository.save(product);
+                }
+        ).orElseGet(() -> {
+            newProduct.setId(id);
+            return repository.save(newProduct);
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("OK", "OK", updateProduct)
+        );
+    }
+    @DeleteMapping("/{id}")
+    ResponseEntity<ResponseObject> deleteProduct(@PathVariable Long id) {
+        boolean existId = repository.existsById(id);
+        if(existId)
+        {
+            repository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                    new ResponseObject("OK", "delete OK", "")
+            );
+        }
+        else
+        {
+            repository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new ResponseObject("fail", "Id not found", "")
+            );
+        }
     }
 }
--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repositories;
 import com.example.demo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByProductName(String name);
 }
